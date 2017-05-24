23cafee67aeaafb6d5327f83eff282c95fed156e
Your Name <you@example.com>
2017-05-25 00:04:01 +0200

    Début du projet minisite

---
+++ page_acc.php
@@ -0,0 +1,39 @@
+<?php ?>
+<!DOCTYPE html>
+<!--
+Projet: MINISITE
+JUIN 2017
+HILLEWAERT Daniel
+-->
+
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/general.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <main>
+            <nav>
+                <div class="nav_1">A</div>
+                <div class="nav_2">B</div>
+                <div class="nav_3">C</div>
+                <div class="nav_4">D</div>
+            </nav>
+            <header>
+                <div class="logo_img"><img src="img/logoA.png" alt="logo"/></div>
+                <div class="logo_txt"></div>
+                <ul>
+                    <li>L1</li>
+                    <li>L2</li>
+                    <li>L3</li>
+                    <li>L4</li>
+                </ul>
+            </header>
+            <section>
+                Section
+            </section>
+        </main>
+    </body>
+</html>
