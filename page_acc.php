382bf54bdee73a0df0f1b04260a79dbfe5caf087
Your Name <you@example.com>
2017-05-25 14:03:11 +0200

    Commit à partir de NetBeans

--- page_acc.php
+++ page_acc.php
@@ -34,6 +34,9 @@ HILLEWAERT Daniel
             <section>
                 Section
             </section>
+            <div class="localisation">
+                <script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script><div style='overflow:hidden;height:450px;width:700px;'><div id='gmap_canvas' style='height:450px;width:700px;'></div><div><small><a href="http://embedgooglemaps.com">google maps carte</a></small></div><div><small><a href="http://youtubeembedcode.com">embed youtube code</a></small></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div><script type='text/javascript'>function init_map(){var myOptions = {zoom:10,center:new google.maps.LatLng(50.6432505,5.560698600000023),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(50.6432505,5.560698600000023)});infowindow = new google.maps.InfoWindow({content:'<strong>liege Saint Laurent</strong><br> Rue St Laurent 29, 4000 Liège<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+            </div>
         </main>
     </body>
 </html>
