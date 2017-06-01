abbe41f6b33dc82ae55777792376e71623e41556
Your Name <you@example.com>
2017-06-01 21:22:07 +0200

    Commit NetBeans après le cours du 1/6

--- page_acc.php
+++ page_acc.php
@@ -16,23 +16,22 @@ HILLEWAERT Daniel
     <body>
         <main>
             <nav>
-		<div class="nav"><a href="http://www.isl.be/"><img src="img/facebook.png" alt="facebook" /></a></div>
+		<!--<div class="nav"><a href="http://www.isl.be/"><img src="img/facebook.png" alt="facebook" /></a></div>-->
+		<div class="nav"><img src="img/facebook.png" alt="facebook" /></div>
                 <div class="nav"><a href="http://www.isl.be/"><img src="img/twitter.png" alt="twitter" /></a></div>
                 <div class="nav"><a href="http://www.isl.be/"><img src="img/linkedin.png" alt="linkedin" /></a></div>
                 <div class="nav"><a href="http://www.isl.be/"><img src="img/google.png" alt="google" /></a></div>
             </nav>
             <header>
-		<!--<div class="box_liens">-->
-		    <div class="lien"><a href="page_acc.php">A PROPOS</a></div>
-		    <div class="lien"><a href="pg_portfolio.php">PORTFOLIO</a></div>
-		    <div class="lien"><a href="pg_cv.php">CV</a></div>
-		    <div class="lien"><a href="pg_contacts.php">CONTACTS</a></div>
-		<!--</div>-->
 		<div class="logo_img"><img src="img/logoA.png" alt="logo" /></div>
                 <div class="logo_txt">
                     <div class="txt_nom">John Doe</div>
                     <span class="txt_fonction">Architecte-Urbaniste</span>
                 </div>
+		<div class="lien-1"><a href="page_acc.php"><div>A PROPOS</div><div class="lien_bis_img_1">--<img src="img/about.png" alt="A propos" />--</div><div class="lien_bis_txt_1">De moi</div></a></div>
+		<div class="lien-2"><a href="pg_portfolio.php"><div>PORTFOLIO</div><div class="lien_bis_img_2">--<img src="img/portfolio.png" alt="Portfolio" />--</div><div class="lien_bis_txt_2">Mes Travaux</div></a></div>
+		<div class="lien-3"><a href="pg_cv.php"><div>CV</div><div class="lien_bis_img_3">--<img src="img/resume.png" alt="CV" />--</div><div class="lien_bis_txt_3">Mon profil</div></a></div>
+		<div class="lien-4"><a href="pg_contacts.php"><div>CONTACTS</div><div class="lien_bis_img_4">--<img src="img/contacts.png" alt="Contacts" />--</div><div class="lien_bis_txt_4">Contactez-moi</div></a></div>
             </header>
             <section>
                 <div class="sec_img"><img src="img/me-2.jpg" alt="personnage" /></div>
