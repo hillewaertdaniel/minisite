651ba7bbe3fe240525e8193dc06926d1e10af026
Your Name <you@example.com>
2017-06-14 17:19:07 +0200

    Page 2 complète

--- page_2.php
+++ page_2.php
@@ -1,12 +1,61 @@
 <?php
-    include("pg_haut.php");
+include("pg_haut.php");
 ?>
-    <div class="bloc_pg_2_A"><div class="_end">bloc_pg_2_A</div></div>
-    <div class="bloc_pg_2_B"><div class="_end">bloc_pg_2_B</div></div>
-    <div class="bloc_pg_2_C"><div class="_end">bloc_pg_2_C</div></div>
-    <div class="bloc_pg_2_D"><div class="_end">bloc_pg_2_D</div></div>
-    <div class="bloc_pg_2_E"><div class="_end">bloc_pg_2_E</div></div>
-    <div class="bloc_pg_2_F"><div class="_end">bloc_pg_2_F</div></div>
+<div class="bloc_pg_2_A">
+    <div class="transition_bloc_pg_2_A">
+	<div class="titre_bloc_pg_2_A">Extension</div>
+	<div class="texte_bloc_pg_2_A">
+	    Une ancienne annexe agricole est accolée à une maison unifamiliale.
+	    Le projet consiste à faire une extension à la grange afin d’en faire un logement supplémentaire.<br>
+	    <a href="#">A découvrir</a>
+	</div>
+    </div>
+</div>
+<div class="bloc_pg_2_B">
+    <div class="transition_bloc_pg_2_B">
+	<div class="titre_bloc_pg_2_B">Modelisation + Rendu 3D</div>
+	<div class="texte_bloc_pg_2_B">
+	    Modification / optimisation des rendus 3D pour le nouveau catalogue de la société PLASSE (spécialisée dans la réalisation d’escaliers sur mesure / fabrication Française!)<br>
+	    <a href="#">A découvrir</a>
+	</div>	    
+    </div>
+</div>
+<div class="bloc_pg_2_C">
+    <div class="transition_bloc_pg_2_C">
+	<div class="titre_bloc_pg_2_C">Design intérieur</div>
+	<div class="texte_bloc_pg_2_C">
+	    Un espace doit être beau, harmonieux, mais aussi fonctionnel.<br>
+	    <a href="#">A découvrir</a>
+	</div>	    
+    </div>
+</div>
+<div class="bloc_pg_2_D">
+    <div class="transition_bloc_pg_2_D">
+	<div class="titre_bloc_pg_2_D">Construction</div>
+	<div class="texte_bloc_pg_2_D">
+	    Construction d’une école maternelle pour 168 enfants avec une conciergerie et des équipements sportifs à Neder-Over-Heembeek.<br>
+	    <a href="#">A découvrir</a>
+	</div>	    
+    </div>
+</div>
+<div class="bloc_pg_2_E">
+    <div class="transition_bloc_pg_2_E">
+	<div class="titre_bloc_pg_2_E">Amenagement</div>
+	<div class="texte_bloc_pg_2_E">
+	    Aménagement des abords met en scène une progression d'éléments végétaux et minéraux afin d'organiser des espaces d'ambiances différentes.<br>
+	    <a href="#">A découvrir</a>
+	</div>	    
+    </div>
+</div>
+<div class="bloc_pg_2_F">
+    <div class="transition_bloc_pg_2_F">
+	<div class="titre_bloc_pg_2_F">Rénovation</div>
+	<div class="texte_bloc_pg_2_F">
+	    L’intervention se situe au centre-ville en intérieur d’ilot, à l’arrière d’une maison .Un ancien atelier s’est effondré, ne laissant que les murs et la dalle de cave. Les travaux consistent à reconstruire un logement de 4 chambres<br>
+	    <a href="#">A découvrir</a>
+	</div>	    
+    </div>
+</div>
 <?php
-    include("pg_bas.php");
+include("pg_bas.php");
 ?>
