3e8f0eb200793811617e69640848c937dd1030e5
Your Name <you@example.com>
2017-06-15 00:00:11 +0200

    page 3 A

--- page_3.php
+++ page_3.php
@@ -1,8 +1,76 @@
 <?php
-    include("pg_haut.php");
+include("pg_haut.php");
 ?>
-    <div class="bloc_pg_3_A"><div class="_end">bloc_pg_3_A</div></div>
-    <div class="bloc_pg_3_B"><div class="_end">bloc_pg_3_B</div></div>
+<div class="bloc_pg_3_A">
+    <div class="titre_bloc_pg_3_A">Expérience professionnelle</div>
+
+    <div class="experience_bloc_pg_3_A">
+
+	<div class="job_bloc_pg_3_A">
+	    <div class="img_bloc_pg_3_A"><img src="img/resume_smaller.png"></div>
+	    <div class="date_bloc_pg_3_A">2010 - Now</div>
+	    <div class="nom_bloc_pg_3_A">
+		<p><strong>ACB Epure</strong></p>
+		Lorem ipsum
+	    </div>
+	    <div class="clr"></div>
+	    <p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+	    </p>
+	</div>
+
+	<div class="job_bloc_pg_3_A">
+	    <div class="img_bloc_pg_3_A"><img src="img/resume_smaller.png"></div>
+	    <div class="date_bloc_pg_3_A">2007 - 2010</div>
+	    <div class="nom_bloc_pg_3_A">
+		<p><strong>Le 18bis</strong></p>
+		Lorem ipsum dolor
+	    </div>
+	    <div class="clr"></div>
+	    <p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,.
+	    </p>
+	</div>
+
+	<div class="job_bloc_pg_3_A">
+	    <div class="img_bloc_pg_3_A"><img src="img/resume_smaller.png"></div>
+	    <div class="date_bloc_pg_3_A">2006</div>
+	    <div class="nom_bloc_pg_3_A">
+		<p><strong>Easao11</strong></p>
+		Lorem
+	    </div>
+	    <div class="clr"></div>
+	    <p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.
+	    </p>
+	</div>
+
+	<div class="job_bloc_pg_3_A">
+	    <div class="img_bloc_pg_3_A"><img src="img/resume_smaller.png"></div>
+	    <div class="date_bloc_pg_3_A">2000 - 2006</div>
+	    <div class="nom_bloc_pg_3_A">
+		<p><strong>ARM</strong></p>
+		Lorem ipsum amet
+	    </div>
+	    <div class="clr"></div>
+	    <p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+	    </p>
+	</div>
+
+    </div>
+
+</div>
+
+<div class="bloc_pg_3_B">
+    <div class="titre_bloc_pg_3_B">Logiciels</div>
+    <div class="logiciel_bloc_pg_3_B">
+	AutoCAD
+	<p>
+	    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+	</p>
+    </div>
+</div>
 <?php
-    include("pg_bas.php");
+include("pg_bas.php");
 ?>
--- page_4.php
+++ page_4.php
@@ -1,9 +1,15 @@
 <?php
-    include("pg_haut.php");
+include("pg_haut.php");
 ?>
-    <div class="bloc_pg_4_A"><div class="_end">bloc_pg_4_A</div></div>
-    <div class="bloc_pg_4_B"><div class="_end">bloc_pg_4_B</div></div>
-    <div class="bloc_pg_4_C"><div class="_end">bloc_pg_4_C</div></div>
+<div class="bloc_pg_4_A">
+    <div class="gooble_bloc_pg_4_A">Google</div>
+</div>
+<div class="bloc_pg_4_B">
+    <div class="experience_bloc_pg_4_B">contact</div>
+</div>
+<div class="bloc_pg_4_C">
+    <div class="logiciels_bloc_pg_4_C"></div>
+</div>
 <?php
-    include("pg_bas.php");
+include("pg_bas.php");
 ?>
